f976335e60badad41ea20e3594a53bc1ed622d85
zirothedev <obotdavid02@gmail.com>
2025-06-17 22:59:29 -0500

    Update settings.py

--- bellus_naturale/settings.py
+++ bellus_naturale/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-me-in-producti
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
-ALLOWED_HOSTS = ['bellus.onrender.com']
+ALLOWED_HOSTS = ['bellus-556p.onrender.com']
 
 # Application definition
 DJANGO_APPS = [
